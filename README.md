d804f7aca9fd4f058e7fc51569779ee7804dd6d4
jasonlav <accounts@erality.com>
2014-08-27 10:58:40 -0600

    License, Readme, and Updated functionality
    MIT licenses added. Detailed readme included. Script is modified to
    work in IE8 and to be flexible enough for HTML5 history api, Hash tag,
    or other navigation methods.

--- README.md
+++ README.md
@@ -1,4 +1,135 @@
-navigate-js
-===========
+#navigate-js
+####Parses links and attaches click events for HTML5 push state, Hash tags, and other navigation methods.
 
-Parses links and attaches click events for HTML5 push state
+Installation
+------------
+
+Include navigate.min.js to your HTML file; this library has no dependencies. Library should be loaded at the bottom of the HTML body.
+
+``` html
+<script src="navigate.js"></script>
+```
+
+Usage
+-----
+
+#####Simple
+``` javascript
+var config = {};
+var filter = function(element) {
+	if(element.tagName === "A") {
+		return true;
+	}
+	return false;
+};
+var callback = function(href, e) {
+	alert(href);
+};
+
+var nav = new Navigate(filter, callback, config);
+nav.parseLinks(document.getElementById("wrapper"));
+```
+``` html
+<div id="wrapper">
+	<a href="example1">Example 1</a>
+	<a href="example2">Example 2</a>
+</div>
+```
+
+#####Backbone style hash tag example
+``` javascript
+var config = {
+	"removeHrefPrefix": ["#/"],
+	"preventDefault": false
+};
+var filter = function(element) {
+	if(element.tagName === "A" && element.getAttribute("href").substr(0, 2) === "#/") {
+		return true;
+	}
+	return false;
+};
+var callback = function(href, e) {
+	alert(href);
+};
+
+var nav = new Navigate(filter, callback, config);
+nav.parseLinks(document.getElementById("wrapper"));
+```
+
+```html
+<div id="wrapper">
+	<a href="/test">test</a>
+</div>
+```
+
+#####HTML5 History API Example
+``` javascript
+var config = {
+	"removeHrefPrefix": ["/"]
+};
+var filter = function(element) {
+	if(element.tagName === "A" && element.getAttribute("href").substr(0, 1) === "/") {
+		return true;
+	}
+	return false;
+};
+var callback = function(href, e) {
+	//Call history api...
+	alert(href);
+};
+
+var nav = new Navigate(filter, callback, config);
+nav.parseLinks(document.getElementById("wrapper"));
+```
+
+```html
+<div id="wrapper">
+	<a href="/test">test</a>
+</div>
+```
+
+Options
+-----
+#####filter
+A function called on parsed elements and children found via config.selector. The element is based as the first parameter of the function. Function returns true or false based on if the element should be handled by navigate.js or not. Required.
+
+#####callback
+A function called when the user triggers the navigate event (typically click). First parameter passed is the href (may be modified by config.removeHrefPrefix); second parameter is the event object. Required.
+
+#####default config
+``` javascript
+{
+	"linkEvent": "click",
+	"selector": "a",
+	"ignoreHrefPrefix": ["http://", "https://", "mailto:"],
+	"ignoreTarget": ["_blank"],
+	"preventDefault": true
+}
+```
+
+#####config.linkEvent
+Event type to attach to element. Default is "click".
+
+#####config.selector
+Selector (querySelectorAll) to search for children when parsing links. Default is "a".
+
+#####config.ignoreHrefPrefix
+Ignore elements with matching href prefixes. Default is "http://", "https://", "mailto". Typically, these prefixes indicate that the link is going to an external page and should not be captured by navigate.js.
+
+#####config.ignoreTarget (array of strings)
+Ignore elements with match targets. Default is "_blank". Typically, these targets indicate that the link is going to an external page and should not be captured by navigate.js.
+
+#####config.removeHrefPrefix (array of strings)
+When using hash tags, the prefix (commonly "#' or "#/") will be included in the href returned to the callback. This option allows a simple way to remove these unnecessary prefixes. Note that prefixes are removed in order; once a match is found the following prefixes will be ignored.
+
+#####config.preventDefault (true | false)
+Prevent default behavior of event with necessary IE fallbacks. Default is true. Typically disabled on hash tag method.
+
+Compatibility
+-------------
+Path.js has been tested in all modern browsers and Internet Explorer 8+.
+
+License
+-------------
+Released under [MIT License].
+[MIT License]: http://mit-license.org/
--- navigate.js
+++ navigate.js
@@ -1,6 +1,19 @@
-function Navigate(config, callback) {
-	this.config = config;
+function Navigate(filter, callback, config) {
+	var defaultConfig = {
+		"linkEvent": "click",
+		"selector": "a",
+		"ignoreHrefPrefix": ["http://", "https://", "mailto:"],
+		"ignoreTarget": ["_blank"],
+		"preventDefault": true
+	};
+
+	if(typeof config !== "object") {
+		config = {};
+	}
+
+	this.config = this.extend({}, defaultConfig, config);
 	this.callback = callback;
+	this.filter = filter;
 	this.canChange = true;
 }
 
@@ -24,25 +37,28 @@ Navigate.prototype.disable = function() {
 Navigate.prototype.parseLinks = function(element) {
 	var that = this;
 
-	//Only parse links on browser that support HTML5 History API (push state)
-	if(window.history && window.history.pushState) {
-		var callback = function(e) {
-			that.parseLinkCallback(e, that);
-		};
+	var callback = function(e) {
+		that.parseLinkCallback(e, this);
 
-		//If element contains linkClass, add event
-		if(element.classList.contains(this.config.linkClass)) {
-			element.classList.remove(this.config.linkClass);
+		//Prevent default
+		if(that.config.preventDefault) {
+			if(typeof e === "object" && typeof e.preventDefault === "function") {
+				e.preventDefault();
+			} else {
+				e.returnValue = false;
+			}
+		}
+	};
 
-			element.addEventListener(this.config.linkEvent, callback);
-		} else {
-		//Element does not contain linkClass, query all children and add event
-			var elements = element.querySelectorAll("."+this.config.linkClass);
-			var length = elements.length;
+	if(this.filter(element) && this.exceptions(element)) {
+		this.addEvent(element, this.config.linkEvent, callback);
+	} else {
+		var elements = element.querySelectorAll(this.config.selector);
+		var length = elements.length;
 
-			for(var i=0; i<length; i++) {
-				elements[i].classList.remove(this.config.linkClass);
-				elements[i].addEventListener(this.config.linkEvent, callback);
+		for(var i=0; i<length; i++) {
+			if(this.filter(elements[i]) && this.exceptions(elements[i])) {
+				this.addEvent(elements[i], this.config.linkEvent, callback);
 			}
 		}
 	}
@@ -51,44 +67,96 @@ Navigate.prototype.parseLinks = function(element) {
 /**
  * Parse link callback
  */
-Navigate.prototype.parseLinkCallback = function(e, that) {
-	var element = e.target;
+Navigate.prototype.parseLinkCallback = function(e, element) {
 	var href = element.getAttribute("href");
-	var target = element.getAttribute("target");
+	var length, prefix, i;	
+	
+	//Remove base path
+	if(this.config.removeHrefPrefix) {
+		length = this.config.removeHrefPrefix.length;
 
-	//Ignore prefixes
-	var length = that.config.ignorePrefixes.length;
-	var prefix;
-
-	for(var i=0; i<length; i++) {
-		prefix = that.config.ignorePrefixes[i];
-		if(href.substr(0, prefix.length) === prefix) {
-			return;
-		}
-	}
-
-	//Ignore target
-	if(typeof target === "string") {
-		length = that.config.ignoreTarget.length;
 		for(i=0; i<length; i++) {
-			prefix = that.config.ignoreTarget[i];
+			prefix = this.config.removeHrefPrefix[i];
 
-			if(target.substr(0, prefix.length) === prefix) {
-				return;
+			if(href.substr(0, prefix.length) === prefix) {
+				href = href.substr(prefix.length);
+				break;
 			}
 		}
 	}
 
-	//Prevent default
-	e.preventDefault();
-	
-	//Remove base path
-	if(href.substr(0, that.config.base.length) === that.config.base) {
-		href = href.substr(that.config.base.length);
+	//Check canChange, trigger callback
+	if(this.canChange) {
+		this.callback(href, e);
+	}	
+};
+
+/**
+ * Exceptions
+ */
+Navigate.prototype.exceptions = function(element) {
+	var href = element.getAttribute("href");
+	var target = element.getAttribute("target");
+	var length, prefix, i;
+
+	//Ignore prefixes
+	if(this.config.ignoreHrefPrefix) {
+		length = this.config.ignoreHrefPrefix.length;
+		
+		for(i=0; i<length; i++) {
+			prefix = this.config.ignoreHrefPrefix[i];
+
+			if(href.substr(0, prefix.length) === prefix) {
+				return false;
+			}
+		}
 	}
 
-	//Check canChange, trigger callback
-	if(that.canChange) {
-		that.callback(href, e);
+	//Ignore target
+	if(typeof target === "string" && this.config.ignoreTarget) {
+		length = this.config.ignoreTarget.length;
+		for(i=0; i<length; i++) {
+			prefix = this.config.ignoreTarget[i];
+
+			if(target.substr(0, prefix.length) === prefix) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+};
+
+/**
+ * Extend
+ */
+Navigate.prototype.extend = function(out) {
+	out = out || {};
+
+	for (var i = 1; i < arguments.length; i++) {
+		if (!arguments[i]) {
+			continue;
+		}
+
+		for (var key in arguments[i]) {
+			if (arguments[i].hasOwnProperty(key)) {
+				out[key] = arguments[i][key];
+			}
+		}
+	}
+
+	return out;
+};
+
+/**
+ * Add event listener
+ */
+Navigate.prototype.addEvent = function(el, eventName, handler) {
+	if (el.addEventListener) {
+		el.addEventListener(eventName, handler);
+	} else {
+		el.attachEvent('on' + eventName, function(e){
+			handler.call(el, e);
+		});
 	}
 };
